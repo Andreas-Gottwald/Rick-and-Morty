6f85cadde96a2e155a65829d05f1915e73bbf325
Andreas Gottwald <andreas.gottwald@hotmail.de>
2024-03-21 16:09:24 +0100

    rick-and-morty-fin

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,1 +1,26 @@
-export function CharacterCard() {}
+import { cardContainer } from "../../index.js";
+export function CharacterCard(imgUrl, title, status, type, occurrences) {
+  const card = document.createElement("li");
+  card.innerHTML = `<div class="card__image-container">
+    <img
+      class="card__image"
+      src="${imgUrl}"
+      alt="${title}"
+    />
+    <div class="card__image-gradient"></div>
+  </div>
+  <div class="card__content">
+    <h2 class="card__title">${title}</h2>
+    <dl class="card__info">
+      <dt class="card__info-title">Status</dt>
+      <dd class="card__info-description">${status}</dd>
+      <dt class="card__info-title">Race</dt>
+      <dd class="card__info-description">${type}</dd>
+      <dt class="card__info-title">Occurrences</dt>
+      <dd class="card__info-description">${occurrences}</dd>
+    </dl>
+  </div>`;
+  card.classList.add("card");
+  cardContainer.append(card);
+  return card;
+}
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const cardContainer = document.querySelector('[data-js="card-container"]');
+import { CharacterCard } from "./components/CharacterCard/CharacterCard.js";
+
+export const cardContainer = document.querySelector(
+  '[data-js="card-container"]'
+);
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
@@ -9,6 +13,69 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
+const maxPage = 42;
+let page = 1;
+let searchQuery = "";
+
+export async function fetchCharacters() {
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/?page=${page}&name=${searchQuery}`
+  );
+  const data = await response.json();
+  cardContainer.innerHTML = "";
+  return data.results;
+}
+
+const data = await fetchCharacters();
+
+data.forEach((person) => {
+  CharacterCard(
+    person.image,
+    person.name,
+    person.status,
+    person.species,
+    person.episode.length
+  );
+});
+
+async function renderCards() {
+  await fetchCharacters();
+  const newPersons = await fetchCharacters();
+  newPersons.forEach((person) => {
+    CharacterCard(
+      person.image,
+      person.name,
+      person.status,
+      person.species,
+      person.episode.length
+    );
+  });
+  console.log(newPersons);
+}
+
+renderCards();
+
+nextButton.addEventListener("click", () => {
+  if (page == maxPage) return;
+  page++;
+  renderCards();
+  pagination.textContent = `${page}/${maxPage}`;
+});
+
+prevButton.addEventListener("click", () => {
+  if (page == 1) return;
+  page--;
+  renderCards();
+  pagination.textContent = `${page}/${maxPage}`;
+});
+
+searchBar.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  searchQuery = data.query;
+  renderCards();
+  page = 1;
+  pagination.textContent = `${page}/${maxPage}`;
+  console.log(data.query);
+});
